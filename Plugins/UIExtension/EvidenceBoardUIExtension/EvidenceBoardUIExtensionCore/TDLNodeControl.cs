4fdcbdb7d7d1814ab5c41415c69e5c41f2e58167
abstractspoon <abstractspoon@users.noreply.github.com>
2026-08-01 19:59:07 +1000

    Merge task title render consistency fixes from ToDoList_Dev
    Fix text clipping

--- Plugins/UIExtension/EvidenceBoardUIExtension/EvidenceBoardUIExtensionCore/TDLNodeControl.cs
+++ Plugins/UIExtension/EvidenceBoardUIExtension/EvidenceBoardUIExtensionCore/TDLNodeControl.cs
@@ -1730,9 +1730,11 @@ namespace EvidenceBoardUIExtension
 			DrawTaskIcon(graphics, taskItem, taskRect, backColor);
 			DrawTaskImageExpansionButton(graphics, taskItem, taskRect, backColor);
 
-			// Title
+			// Text, using 'TextRenderer' for consistency with the core app
 			var titleRect = CalcTaskLabelRect(taskItem, taskRect, true);
-			graphics.DrawString(taskItem.ToString(), GetTaskLabelFont(taskItem), new SolidBrush(textColor), titleRect);
+			var flags = (TextFormatFlags.Top | TextFormatFlags.WordBreak | TextFormatFlags.EndEllipsis);
+
+			TextRenderer.DrawText(graphics, taskItem.ToString(), GetTaskLabelFont(taskItem), titleRect, textColor, flags);
 
 			// Image
 			if (taskItem.IsImageExpanded)
